4dac962bd8b189daa87401d9d165fdb8ae8a276a
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2021-09-01 00:13:57 +0200

    qemu-run-zynqmp-xilinx: Update PCIe setup
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- qemu-run-zynqmp-xilinx.sh
+++ qemu-run-zynqmp-xilinx.sh
@@ -18,7 +18,7 @@ HW_DTB=${HOME}/dts/LATEST/SINGLE_ARCH/zcu102-arm.dtb
 KERNEL=arch/arm64/boot/Image
 DTB=arch/arm64/boot/dts/xilinx/zynqmp-zcu102-rev1.0.dtb
 
-PCIE="-device xlnx-pcie-rp,bus=pcie.0,id=pcie.1,port=1,chassis=1 -device pci-bridge,addr=00.0,bus=pcie.1,id=pcie.2,chassis_nr=2 -netdev user,id=hostnet1,net=10.0.3.0/24 -device rtl8139,bus=pcie.2,addr=01.0,netdev=hostnet1,romfile="
+PCIE="-device xlnx-pcie-rp,bus=pcie.0,id=pcie.1,port=1,chassis=1 -device pci-bridge,addr=00.0,bus=pcie.1,id=pcie.2,chassis_nr=2 -netdev user,id=hostnet1,net=10.0.3.0/24 -device e1000e,bus=pcie.2,addr=01.0,netdev=hostnet1,romfile="
 
 RESET_APU="-device loader,addr=0xfd1a0104,data=0x8000000e,data-len=4"
 
